71587af247b617be6273a4fd05286fe10773973a
Felipe Fonseca Vidal Prado <113259593+FelpsFonseca@users.noreply.github.com>
2025-09-18 14:25:27 -0300

    Commit da classe conta

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,4 +1,61 @@
 package cliente;
+import computador.Computador;
 
 public class Cliente {
+    private String nome;
+    private int cpf;
+
+    private Computador[] carrinho;
+    private int qnt;
+
+    public Cliente(String nome, int cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.carrinho = new Computador[20];
+        this.qnt = 0;
+    }
+
+    public void comprar(Computador pc) {
+        if (pc == null) return;
+        if (qnt < carrinho.length) {
+            carrinho[qnt] = pc;
+            qnt++;
+        } else {
+            System.out.println("Carrinho cheio");
+        }
+    }
+
+    public void mostraCarrinho() {
+        if (qnt == 0) {
+            System.out.println("Carrinho vazio");
+            return;
+        }
+        for (int i = 0; i < qnt; i++) {
+            System.out.println("\nItem " + (i + 1) + ":");
+            carrinho[i].mostraPCConfigs();
+        }
+    }
+
+    public double calculaTotalCompra() {
+        double total = 0.0;
+        for (int i = 0; i < qnt; i++) {
+            total += carrinho[i].getPreco();
+        }
+        return total;
+    }
+
+    public void mostraInfo() {
+        System.out.println("Cliente: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Quantidade de PCs: " + qnt);
+    }
+
+    // Getter ProcessarPedido
+    public Computador[] getCarrinho() {
+        Computador[] usados = new Computador[qnt];
+        for (int i = 0; i < qnt; i++) {
+            usados[i] = carrinho[i];
+        }
+        return usados;
+    }
 }
