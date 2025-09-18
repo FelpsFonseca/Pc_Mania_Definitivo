8885cc07b8b017b801fa1ff38cd39d4b82afbf7b
Felipe Fonseca Vidal Prado <113259593+FelpsFonseca@users.noreply.github.com>
2025-09-18 14:28:19 -0300

    Commit classe ProcessarPedido

--- src/processarpedido/ProcessarPedido.java
+++ src/processarpedido/ProcessarPedido.java
@@ -1,4 +1,9 @@
 package processarpedido;
 
+import computador.Computador;
+
 public class ProcessarPedido {
+    public static void enviar(Computador[] pcsDoCliente) {
+        System.out.println("\nPedido enviado...");
+    }
 }
